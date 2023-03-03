aab2c1a781c357aaca528352f516224854c6f3a3
Marcela Quezada <marcela.quezada.araya@gmail.com>
2023-03-02 21:34:12 -0300

    correcion url

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -6,9 +6,40 @@ import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import logo from "../img/logo.png"
 import Button from 'react-bootstrap/esm/Button';
-
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { agregarProducto} from"../../services/product.service";
 
 const AddProduct = () => {
+  const nombre = useRef(null);
+  const descripcion = useRef(null);
+  const stock = useRef(null);
+  const status = useRef(null);
+  const precio = useRef(null);
+  const url = useRef(null);
+  const navigate = useNavigate();
+
+  
+  const onSubmit = async () => {
+  
+
+    const np = nombre.current.value;
+    const desc = descripcion.current.value;
+    const st = stock.current.value;
+    const stat = status.current.value;
+    const pri = precio.current.value;
+    const ur = url.current.value;
+    
+
+
+
+    const agregadoConExito = await agregarProducto(np, desc, st, stat,pri,ur);
+    if (agregadoConExito){
+      navigate("/catalogo")
+    }
+  };
+  
+
  return (
    <Container fluid>
      <Row>
@@ -18,46 +49,47 @@ const AddProduct = () => {
 <form className='w-50'>
 <Form.Group className="mb-3">
        <Form.Label className='text-add'>Nombre</Form.Label>
-       <Form.Control   />
+       <Form.Control  ref = {nombre} />
      
 
-        <Form.Label className='text-add'>Nombre</Form.Label>
-        <Form.Control   />
+       
       </Form.Group>
 
      <Form.Group className="mb-3">
        <Form.Label className='text-add'>Descripción</Form.Label>
-       <Form.Control   />
+       <Form.Control  ref = {descripcion} />
      </Form.Group>
 
 
      <Form.Group className="mb-3">
        <Form.Label className='text-add'>Stock</Form.Label>
-       <Form.Control   />
+       <Form.Control  ref = {stock} />
      </Form.Group>
 
 
      <Form.Group className="mb-3">
        <Form.Label className='text-add'>Status</Form.Label>
-       <Form.Control   />
+       <Form.Control  ref = {status} />
      </Form.Group>
 
 
      <Form.Group className="mb-3">
        <Form.Label className='text-add'>Precio</Form.Label>
-       <Form.Control   />
+       <Form.Control ref = {precio}  />
      </Form.Group>
 
 
      <Form.Group className="mb-3">
        <Form.Label className='text-add'>URL imagen</Form.Label>
-       <Form.Control   />
+       <Form.Control  ref = {url} />
      </Form.Group>
      </form>
     
-     <Button className="boton-comprar" type="submit" size="sm" href='/addproduct'>
+     <Button className="boton-comprar" onClick = {onSubmit} size="sm">
                agregar producto
              </Button>
+            
+
      </Col>
      </center>
 
---
+++ src/services/product.service.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+const addProduct_API = async (nombre, descripcion, stock, status, precio, url) => {
+    const datos = {
+      name: nombre,
+      description: descripcion,
+      status: status,
+      stock:stock,
+      price:precio,
+      img:url
+    };
+    try {
+      const result = await axios.post(`${baseURL}/product/addProduct`, datos, {
+      withCredentials: true,
+      });
+      return result;
+    } catch (error) {
+      return false;
+    }
+  };
+const agregarProducto = async (nombre, descripcion, stock, status, precio, url) => {
+    if (!nombre || !descripcion || !stock || !status || !precio ||!url) {
+      return false;
+    }
+  
+   
+    const result = await addProduct_API(nombre, descripcion, stock, status,precio,url);
+  
+    if (result) {
+      return true;
+    }
+    return false;
+  };
+
+  export { agregarProducto };
