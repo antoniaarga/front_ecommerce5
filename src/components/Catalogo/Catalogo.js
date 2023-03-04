27e1d112169e88c98389759b31b9284a7cdde3c2
Marcela Quezada <marcela.quezada.araya@gmail.com>
2023-03-03 23:04:44 -0300

    revision catologo

--- src/components/Catalogo/Catalogo.js
+++ src/components/Catalogo/Catalogo.js
@@ -48,147 +48,33 @@ console.log(post)
         <img src={banner}/>
         <p className='titulo-seccion pt-5'>Productos más vendidos</p>
         </Row>
+  
         <Row className='row-catalogo'>
+
+        {post.map(item => (
+       <Col sm={6} md={6} lg={3}>
+       <Card className="card-catalogo" >
+         <Card.Img
+           variant="top"
+           className="foto-catalogo"
+           src={item.img}
+         />
+         <Card.Body>
+           <Card.Title className="card-tittle">{item.name}</Card.Title>
+           <Card.Text>
+            {item.description}
+           </Card.Text>
+           <center>
+             <Button size="sm" className="product-button" a href='/product'>
+               Ver Producto
+             </Button>
+           </center>
+         </Card.Body>
+       </Card>
+     </Col>
+      ))}
+
           
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img variant="top" className="foto-catalogo" src={casco1} />
-              <Card.Body>
-                <Card.Title className="card-tittle">Syn Max Air</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img
-                variant="top"
-                className="foto-catalogo"
-                src={teclado1}
-              />
-              <Card.Body>
-                <Card.Title className="card-tittle">Magma Mini</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img
-                variant="top"
-                className="foto-catalogo"
-                src={microfono1}
-              />
-              <Card.Body>
-                <Card.Title className="card-tittle">Torch</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-
-
-        <Row className='row-catalogo'>
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img variant="top" className="foto-catalogo" src={mouse2} />
-              <Card.Body>
-                <Card.Title className="card-tittle">Kone XP Air</Card.Title>
-                <Card.Text className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img variant="top" className="foto-catalogo" src={teclado2} />
-              <Card.Body>
-                <Card.Title className="card-tittle">Syn Max Air</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img
-                variant="top"
-                className="foto-catalogo"
-                src={pack1}
-              />
-              <Card.Body>
-                <Card.Title className="card-tittle">Pack Phantom</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={6} md={6} lg={3}>
-            <Card className="card-catalogo" >
-              <Card.Img
-                variant="top"
-                className="foto-catalogo"
-                src={pack2}
-              />
-              <Card.Body>
-                <Card.Title className="card-tittle">Torch</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <center>
-                  <Button size="sm" className="product-button" a href='/product'>
-                    Ver Producto
-                  </Button>
-                </center>
-              </Card.Body>
-            </Card>
-          </Col>
         </Row>
       </center>
       </Container>
