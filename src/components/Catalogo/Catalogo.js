2e4ac56e30bb826814d58f0580d0bdbdf95f1b0c
Marcela Quezada <marcela.quezada.araya@gmail.com>
2023-03-03 23:36:12 -0300

    sin post

--- src/components/Catalogo/Catalogo.js
+++ src/components/Catalogo/Catalogo.js
@@ -32,7 +32,7 @@ const Catalogo = () => {
       setPost(response.data.products);
       console.log("respuesta api");
     });
-  }, [post]);
+  }, []);
 
   console.log("revision");
   console.log(post);
